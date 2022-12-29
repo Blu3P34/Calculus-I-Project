0f966e29600ad383a9e1bec8e171216bb534c33f
DaoMinhDuy <daominhduy2004@gmail.com>
2022-12-29 21:47:21 +0700

    introduction added

---
+++ main.py
@@ -0,0 +1,9 @@
+from manim import*
+
+class introduction(Scene):
+    def construct(self):
+        intro1 = Text("Calculus I project")
+        intro2 = Text("The application of Calculus I")
+        self.play(Create(intro1), run_time = 2)
+        self.play(ReplacementTransform(intro1,intro2))
+        self.wait()
